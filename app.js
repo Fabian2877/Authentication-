4aff147116de04b22da2412cf4fadbaafc8b141a
Fabian Isom <fabian@Fabians-MacBook-Pro.local>
2020-03-18 19:19:55 -0700

    Level 2

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const app = express(); 
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const encrypt = require("mongoose-encryption")
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/userDB',  { useNewUrlParser: true, useUnifiedTopology: true} )
+
+
+const userSchema = new mongoose.Schema({
+    email: String, 
+    password: String
+})
+
+const secret = 'thisismysecretdonttellanyone';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+
+
+const User = mongoose.model('User', userSchema)
+
+
+
+
+
+app.get('/', function(req, res ) {
+    res.render('home')
+})
+/////////////////////////////////////////////////////////REGISTER////////////////////////////////////////////////////////
+app.get('/register', function(req, res ) {
+    res.render('register')
+})
+
+app.post('/register', function(req, res) {
+    const email = req.body.username
+    const password = req.body.password
+
+    const newUser = new User({
+        email: email, 
+        password: password
+    })
+
+    newUser.save(function(err) {
+        if(!err) {
+            res.render('secrets')
+        } else {
+            console.log('There was an error creating your account')
+        }
+    })
+
+})
+
+
+/////////////////////////////////////////////////////////LOGIN///////////////////////////////////////////////////////////
+
+
+app.get('/login', function(req, res ) {
+    res.render('login')
+})
+
+app.post('/login', function(req, res) {
+    const email = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: email}, function(err, foundUser) {
+        if(err) {
+            console.log(err)
+        } else {
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+      
+    })
+    
+})
+
+
+/////////////////////////////////////////////////////////SECRETS///////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(req, res) {
+    console.log('Your server has started on port 3000')
+})
